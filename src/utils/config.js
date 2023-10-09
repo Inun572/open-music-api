4e6c92a073193fe1d8577a9556811df93a7313d9
Inun572 <helmi.ainun@gmail.com>
2023-10-09 09:01:53 +0700

    try: coba terapkan ini tapi error

---
+++ src/utils/config.js
@@ -0,0 +1,16 @@
+// coba terapkan config, tetapi localhost dan portnya berubah, sehingga pengujian gagal
+
+const config = {
+  app: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+  },
+  rabbitMq: {
+    server: process.env.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: process.env.REDIS_SERVER,
+  },
+};
+
+module.exports = config;
